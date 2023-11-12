589535161696e575b2b54290593c1552179e0489
Ming Hu <113792384+minghu0830@users.noreply.github.com>
2023-11-12 15:58:29 +0800

    Update preprocess_videos.py

--- tools/preprocess_videos.py
+++ tools/preprocess_videos.py
@@ -1,79 +1,42 @@
-import os
 import cv2
-import threading
-from queue import Queue
+import os
+import glob
+from tqdm import tqdm
 
-# Specify the directory to convert
-input_dir = "./dataset/original_videos"
-# Specify the output directory
-output_dir = "./dataset/preprocessed_videos"
-# Specify the FPS and resolution for conversion
-target_fps = 25
-target_width = 256
-target_height = 256
-# Specify the number of concurrent threads
-num_threads = 4
+def change_fps_and_resolution(src_path, dst_path, new_fps, new_width, new_height):
+    cap = cv2.VideoCapture(src_path)
 
-file_queue = Queue()
+    width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps    = cap.get(cv2.CAP_PROP_FPS)
 
-lock = threading.Lock()
+    frame_interval = round(fps / new_fps)
 
-def convert_fps_and_resolution():
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
+    out = cv2.VideoWriter(dst_path, fourcc, new_fps, (new_width, new_height))
+
+    frame_id = 0
     while True:
-        file_path = file_queue.get()
-        if file_path is None:
+        ret, frame = cap.read()
+
+        if not ret:
             break
 
-        cap = cv2.VideoCapture(file_path)
-
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-        if fps == target_fps and width == target_width and height == target_height:
-            lock.acquire()
-            os.system(f"cp {file_path} {os.path.join(output_dir, os.path.relpath(file_path, input_dir))}")
-            lock.release()
-            continue
-
-        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-
-        output_path = os.path.join(output_dir, os.path.relpath(file_path, input_dir))
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        out = cv2.VideoWriter(output_path, fourcc, target_fps, (target_width, target_height))
-
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            frame = cv2.resize(frame, (target_width, target_height))
+        if frame_id % frame_interval == 0:
+            frame = cv2.resize(frame, (new_width, new_height))
             out.write(frame)
 
-        cap.release()
-        out.release()
+        frame_id += 1
 
-        lock.acquire()
-        print(output_path)
-        lock.release()
+    cap.release()
+    out.release()
 
-        file_queue.task_done()
+source_path = "/source_path"
+destination_path = "/output_path"
+files = glob.glob(os.path.join(source_path, "*.mp4"))
 
-for root, dirs, files in os.walk(input_dir):
-    for filename in files:
-        if not filename.endswith(".mp4"):
-            continue
-
-        file_queue.put(os.path.join(root, filename))
-
-threads = []
-for i in range(num_threads):
-    t = threading.Thread(target=convert_fps_and_resolution)
-    t.start()
-    threads.append(t)
-
-file_queue.join()
-
-for i in range(num_threads):
-    file_queue.put(None)
-for t in threads:
-    t.join()
+for file in tqdm(files, desc="Processing videos"):
+    filename = os.path.basename(file)
+    dst_path = os.path.join(destination_path, filename)
+    change_fps_and_resolution(file, dst_path, 25, 256, 256)
